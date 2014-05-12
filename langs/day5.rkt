545b7926f52c9610d2b3265925bc52ed5e19fd72
Trevor Oakes <trevor.oakes@gmail.com>
2014-05-12 14:58:31 -0600

    Reworked Day5, passes existing tests, does not clear stack

--- langs/day5.rkt
+++ langs/day5.rkt
@@ -2,298 +2,280 @@
 (require racket/contract
          racket/match
          racket/set
+         plai/datatype
+         "../substitution-cipher.rkt"
          (prefix-in x86: "asm.rkt"))
 
-;; define app
-;; functions don't return
+#| Add (define app). Functions don't return
+   P  = (D ... K) | K
+   D  = (define (<id> <id> ...4) K)
+   K = (app <id> E ...4) | (if0 E K K) | E
+   E  = <id> | <num> | (<binop> E E) | (<unaop> E)
+   <binop> =  + | - | bitwise-and | bitwise-ior | bitwise-xor
+            | * | quotient | remainder
+            | = | < | <= | > | >=
+   <unaop> = add1 | sub1 | bitwise-not
+   <id> = [^ ()[]{}",'`:#\| ]+ and not a reserved word
+|#
 
-(struct e () #:prefab)
-(struct num e (n) #:prefab)
-
-(struct binop e (opor lhs rhs) #:prefab)
-(struct unaop e (opor opand) #:prefab)
-
-(struct mult e (lhs rhs) #:prefab)
-(struct quot e (lhs rhs) #:prefab)
-(struct rem e (lhs rhs) #:prefab)
-
-(struct boolop e (opor lhs rhs) #:prefab)
-
-(struct if0 e (test trueb falsb) #:prefab)
-
-(struct definition-seq e (defines body) #:prefab)
-(struct definition e (naming variables body) #:prefab) ; limit variables to 4?
-(struct application e (function inputs) #:prefab)
-(struct id e (name) #:prefab)
-
-(define binop-assoc?
-  (match-lambda
-    [(or '- (? (λ (op) (equal? op x86:sub)))) #f]
-    [_ #t]))
-
-(define binops
-  (hash '+ x86:add
-        '- x86:sub
-        'bitwise-and x86:and
-        'bitwise-ior x86:or
-        'bitwise-xor x86:xor))
-(define unaops
-  (hash 'add1 x86:inc
-        'sub1 x86:dec
-        'bitwise-not x86:not))
-
-(define boolops
-  (set '= '< '<= '> '>=))
-
-(define reserved-words
-  (set-union (set '* 'quotient 'remainder 'if0 'define 'app)
-             (list->set boolops)
-             (list->set (hash-keys binops))
-             (list->set(hash-keys unaops))))
-
-
-(define (valid-id? pid) ; enforce which naming convention? [a-zA-Z]?
+(define (valid-id? pid) ;XX enforce slightly stricter than racket symbols
+  #;(and (regexp-match? #rx"[^\\(\\)\\[\\]{}\\\",'`;#\\|\\\\]+" 
+                        (symbol->string pid)))
   (and (symbol? pid) 
-       (not (set-member? reserved-words pid))))
+       (not (or (unaop? pid) (binop? pid) (eq? 'if0 pid) 
+                (eq? 'define pid) (eq? 'app pid)))))
 
-(define parse
+(define-type E
+  [Num (n number?)]
+  [Binop (op binop-src?) (lhs E?) (rhs E?)]
+  [Unaop (opor unaop-src?) (opand E?)]
+  [Id (i valid-id?)])
+
+(define-type K
+  [App 
+   (function Id?) 
+   (inputs (λ (is) (andmap E? is)))]
+  [If0 (test E?) (trueb K?) (falsb K?)]
+  [Return (e E?)])
+
+(define-type D
+  [Def 
+   (naming Id?) 
+   (variables (λ (vs) (andmap Id? vs)))
+   (body K?)])
+
+(define-type P 
+  [Program 
+   (defines (λ (ds) (andmap D? ds)))
+   (body K?)]
+  [P-K (k K?)])
+
+(define (binop-src->asm-helper ->asm)
+  (match-lambda*
+    [(list x86:eax (? x86:register? r))
+     (->asm r)]))
+
+(define (cmpop->asm op)
+  (binop-src->asm-helper
+   (λ (r)
+     (x86:seqn
+      (x86:cmp x86:eax r)
+      #;(x86:mov x86:eax 0)
+      (op x86:al)))))
+
+(define (cmpop->rkt op)
+  (match-lambda* [(list l r) (if (op l r) 1 0)]))
+
+(define-subst-cipher
+  binop
+  [src          rkt         asm]
+  ['+           +           x86:add]
+  ['-           -           x86:sub]
+  ['bitwise-and bitwise-and x86:and]
+  ['bitwise-ior bitwise-ior x86:or]
+  ['bitwise-xor bitwise-xor x86:xor]
+  ['*           *           (binop-src->asm-helper x86:imul)]
+  ['quotient    quotient    (binop-src->asm-helper x86:idiv)]
+  ['remainder   remainder   (binop-src->asm-helper 
+                             (λ (r) 
+                               (x86:seqn
+                                (x86:idiv r)
+                                (x86:mov x86:eax x86:edx))))]
+  ['=       (cmpop->rkt =)  (cmpop->asm x86:sete)]
+  ['<       (cmpop->rkt <)  (cmpop->asm x86:setl)]
+  ['<=      (cmpop->rkt <=) (cmpop->asm x86:setle)]
+  ['>       (cmpop->rkt >)  (cmpop->asm x86:setg)]
+  ['>=      (cmpop->rkt >=) (cmpop->asm x86:setge)])
+
+(define-subst-cipher
+  unaop
+  [src          rkt         asm]
+  ['add1        add1        x86:inc]
+  ['sub1        sub1        x86:dec]
+  ['bitwise-not bitwise-not x86:not])
+
+(define parse-p
   (match-lambda
-    [(list (? (λ (i) #t) (list (? (λ (s) (equal? s 'define))) _ _) defs) ... 
-           app-body)
-     (definition-seq (map parse defs) (parse app-body))]
-    [(list (? (λ (s) (equal? s 'define)))
-           (list (? valid-id? the-id) (? valid-id? ids)...)
-           function-body)
-     (definition (id the-id) (map parse ids) (parse function-body))]
-    [(list (? (λ (s) (equal? s 'app)))
-           (? valid-id? fid)
-           params ...)
-     (application (id fid) (map parse params))]
-    [(list (? (λ (s) (equal? s 'if0))) test tb fb)
-     (if0 (parse test) (parse tb) (parse fb))]
-    [(list (? (λ (s) (set-member? boolops s)) op) lhs rhs)
-     (boolop op (parse lhs) (parse rhs))]
-    [(list (? (λ (s) (equal? s '*))) lhs rhs)
-     (mult (parse lhs) (parse rhs))]
-    [(list (? (λ (s) (equal? s 'quotient))) lhs rhs)
-     (quot (parse lhs) (parse rhs))]
-    [(list (? (λ (s) (equal? s 'remainder))) lhs rhs)
-     (rem (parse lhs) (parse rhs))]
-    [(list (? (λ (s) (and (symbol? s) (hash-has-key? binops s))) 
-              operator)
-           lhs
-           rhs)
-     (binop (hash-ref binops operator) (parse lhs) (parse rhs))]
-    [(list (? (λ (s) (and (symbol? s) (hash-has-key? unaops s)))
-              operator)
-           operand)
-     (unaop (hash-ref unaops operator) (parse operand))]
-    [(? valid-id? i)
-     (id i)]
+    [(list (and (list 'define _ _) defs) ... app-body)
+     (Program (map parse-d defs) (parse-k app-body))]
+    [k-expr (P-K (parse-k k-expr))]))
+
+(define parse-d
+  (match-lambda
+    [(list 'define (list (? valid-id? the-id) (? valid-id? ids) ...) body)
+     (Def (Id the-id) (map Id ids) (parse-k body))]))
+
+(define parse-k
+  (match-lambda
+    [(list 'app (? valid-id? fid) params ...)
+     (App (Id fid) (map parse-e params))]
+    [(list 'if0 test trueb falsb)
+     (If0 (parse-e test) (parse-k trueb) (parse-k falsb))]
+    [expr (Return (parse-e expr))]))
+
+(define parse-e
+  (match-lambda
+    [(list (? binop-src? operator) lhs rhs)
+     (Binop operator (parse-e lhs) (parse-e rhs))]
+    [(list (? unaop-src? operator) operand)
+     (Unaop operator (parse-e operand))]
     [(? byte? b)
-     (num b)]))
+     (Num b)]
+    [(? valid-id? i)
+     (Id i)]))
 
-(define end-label 
-  (x86:make-label 'end))
+(define (Id-name e)
+  (type-case E e
+    [Id (i) i]
+    [Num (b) (error)][Unaop (r d) (error)][Binop (o l r) (error)]))
+(define (Def-name d)
+  (type-case D d [Def (naming vars body) naming]))
 
-(define gamma (make-parameter (hash)))
+(define end-label (x86:make-label 'program-end))
 (define stack-offset (make-parameter 0))
 
-;; eax is default return value
-(define to-asm
-  (parameterize ([gamma (hash)])
-    (match-lambda
-      [(definition-seq defines body)
-       ;; define function labels and add to gamma
-       (parameterize
-           ([gamma (for/fold ([g (gamma)])
-                     ([def defines])
-                     (hash-set g 
-                               (definition-naming def)
-                               (x86:make-label 
-                                (id-name (definition-naming def)))))])
-         (x86:seqn
-          (to-asm body)
-          (apply x86:seqn (map to-asm defines))
-          (x86:label-mark end-label)))]
-      [(definition naming vars body)
-       ;; put [esp+] for vars in gamma
-       
+(define to-asm (λ (pp) (p->asm pp (hash))))
+(define (p->asm pp gamma)
+  (type-case P pp
+    [Program
+     (defs body)
+     (let ([g
+            (for/fold ([g gamma])
+              ([def defs])
+              (hash-set g
+                        (Def-name def)
+                        (x86:make-label
+                         (Id-name (Def-name def)))))])
        (x86:seqn
-        (x86:label-mark (hash-ref (gamma) naming))
-        (parameterize
-            ([gamma (for/fold ([g (gamma)]) 
-                      ([v vars]
-                       [i (in-naturals 0)]);; (in-naturals ?)
-                      (hash-set g 
-                                v 
-                                (cons 'esp+ (* 4 i))))])
-          (to-asm body))
-        (x86:jmp end-label))]
-      [(application function values)
-       (x86:seqn
-        ;; calculate the values to pass with stack
-        
+        (k->asm body g)
         (apply x86:seqn 
-               (for/list ([v (reverse values)]
-                          [so (in-naturals 0)])
+               (for/list ([def defs])
                  (x86:seqn
-                  (parameterize ([stack-offset so])
-                    (to-asm v))
-                  (x86:push x86:eax))))
+                  (x86:label-mark (hash-ref g (Def-naming def)))
+                  (d->asm def g))))
         
-        ;; clear the stack of anything but the new values?
-        
-        (x86:jmp (hash-ref (gamma) function))
-        
-        )]
-      [(id i)
-       (x86:seqn
-        (x86:mov x86:eax ((match-lambda 
-                            [(cons 'esp+ n) (x86:esp+ (+ n (* 4 (stack-offset))))]
-                            [expr expr])
-                          (hash-ref (gamma) (id i)))))]
-      [(if0 test trueb falsb) ; optimize away? not as default
-       (let ([trueb-end (x86:make-label)]
-             [falsb-end (x86:make-label)])
-         (x86:seqn
-          (to-asm test)
-          (x86:cmp x86:eax 0)
-          (x86:jne trueb-end)
-          (to-asm trueb)
-          (x86:jmp falsb-end)
-          (x86:label-mark trueb-end)
-          (to-asm falsb)
-          (x86:label-mark falsb-end)))]
-      [(boolop op l r)
-       (x86:seqn
-        (to-asm r)
-        (x86:mov x86:ebx x86:eax)
-        (to-asm l)
-        (x86:cmp x86:eax x86:ebx)
-        #;(x86:mov x86:eax 0)
-        ((match op
-           ['= x86:sete]
-           ['< x86:setl]
-           ['<= x86:setle]
-           ['> x86:setg]
-           ['>= x86:setge])
-         x86:al))]
-      [(mult l r)
-       (x86:seqn
-        (to-asm l)
-        (x86:mov x86:ebx x86:eax)
-        (to-asm r)
-        (x86:imul x86:ebx))]
-      [(quot l r)
-       (x86:seqn
-        (to-asm r)
-        (x86:mov x86:ebx x86:eax)
-        (to-asm l)
-        #;(x86:cdq)
-        (x86:idiv x86:ebx))]
-      [(rem l r)
-       (x86:seqn
-        (to-asm r)
-        (x86:mov x86:ebx x86:eax)
-        (to-asm l)
-        #;(x86:cdq)
-        (x86:idiv x86:ebx)
-        (x86:mov x86:eax x86:edx))]
-      [(binop o l r)
-       (x86:seqn
-        (to-asm r)
-        (x86:mov x86:ebx x86:eax)
-        (to-asm l)
-        #;(if (binop-assoc? o)
-              (x86:pop x86:ebx)
-              (x86:seqn
-               (x86:mov x86:ebx x86:eax)
-               (x86:pop x86:eax)))
-        (o x86:eax x86:ebx))]
-      [(unaop operator operand)
-       (x86:seqn
-        (to-asm operand)
-        (operator x86:eax))]
-      [(num b)
-       (x86:seqn
-        (x86:mov x86:eax b))])))
+        (x86:label-mark end-label)))]
+    [P-K (k) (k->asm k gamma)]))
 
-(define (x86-op->racket-op o)
-  (cond
-    [(equal? o x86:add) +]
-    [(equal? o x86:sub) -]
-    [(equal? o x86:and) bitwise-and]
-    [(equal? o x86:or) bitwise-ior]
-    [(equal? o x86:xor) bitwise-xor]
-    [(equal? o x86:inc) add1]
-    [(equal? o x86:dec) sub1]
-    [(equal? o x86:not) bitwise-not]))
+(define (d->asm pp gamma)
+  (type-case D pp
+    [Def
+     (naming vars body)
+     (x86:seqn
+      (k->asm body
+              (for/fold ([g gamma])
+                ([v vars]
+                 [n (in-naturals)])
+                (hash-set g v (cons 'stack-pos n))))
+      (x86:jmp end-label))]))
 
-(define (interp prgm)
-  #;(displayln prgm)
-  #;(fprintf (current-output-port) "Gamma: ~a\n" (gamma))
-  #;(displayln "")
-  (match prgm
-    [(definition-seq defines body) ;; make definition-seq work like let*?
-     (parameterize 
-         ([gamma (for/fold ([g (gamma)])
-                   ([def defines])
-                   (hash-set g (definition-naming def) def))])
-       (interp body))]
-    [(definition naming vars body)
-     (error)]
-    [(application function values) ;; remove self recursion?
-     (let ([fd (hash-ref (gamma) function)])
-       (parameterize 
-           ([gamma (for/fold ([g (gamma)])
-                     ([var (map id-name
-                                (definition-variables fd))]
-                      [val (map interp values)])
-                     (hash-set g var val))])
-         (interp (definition-body fd))))]
-    [(if0 test trueb falsb) ;; should if0 be short-circuiting?
-     (if (= 0 (interp test))
-         (interp trueb)
-         (interp falsb))]
-    [(boolop op lhs rhs)
-     (if ((match op
-            ['= =]
-            ['< <]
-            ['<= <=]
-            ['> >]
-            ['>= >=]) (interp lhs) (interp rhs))
-         1
-         0)]
-    [(mult lhs rhs)
-     (* (interp lhs)
-        (interp rhs))]
-    [(quot lhs rhs)
-     (quotient
-      (interp lhs)
-      (interp rhs))]
-    [(rem lhs rhs)
-     (remainder
-      (interp lhs)
-      (interp rhs))]
-    [(binop opor lhs rhs)
-     ((x86-op->racket-op opor)
-      (interp lhs)
-      (interp rhs))]
-    [(unaop opor opand)
-     ((x86-op->racket-op opor)
-      (interp opand))]
-    [(id i)
-     (interp (hash-ref (gamma) i))]
-    [(num b)
-     b]
-    [interped-val
-     interped-val]))
+(define (k->asm pp gamma)
+  (type-case K pp
+    [If0 
+     (test trueb falsb)
+     (let ([falsb-start (x86:make-label 'falsb-start)]
+           [if0-end (x86:make-label 'if0-end)])
+       (x86:seqn
+        (e->asm test gamma)
+        (x86:cmp x86:eax 0)
+        (x86:jne falsb-start)
+        (k->asm trueb gamma)
+        (x86:jmp if0-end)
+        (x86:label-mark falsb-start)
+        (k->asm falsb gamma)
+        (x86:label-mark if0-end)))]
+    [App
+     (function inputs)
+     (x86:seqn
+      ;; calculate and place on the stack the value of each input
+      (apply x86:seqn
+             (for/list ([i (reverse inputs)]
+                        [so (in-naturals)])
+               (x86:seqn
+                (parameterize ([stack-offset so])
+                  (e->asm i gamma))
+                (x86:push x86:eax))))
+      ;;XX clear the stack of anything but the new input values
+      
+      (x86:jmp (hash-ref gamma function)))]
+    [Return (e) (e->asm e gamma)]))
+
+(define (e->asm pp gamma)
+  (type-case E pp
+    [Binop 
+     (op lhs rhs)
+     (x86:seqn
+      (x86:push x86:ebx)
+      (parameterize ([stack-offset (add1 (stack-offset))])
+        (x86:seqn
+         (e->asm rhs gamma)
+         (x86:mov x86:ebx x86:eax)
+         (e->asm lhs gamma)
+         ((binop-src->asm op) x86:eax x86:ebx)))
+      (x86:pop x86:ebx))]
+    [Unaop 
+     (operator operand)
+     (x86:seqn
+      (e->asm operand gamma)
+      ((unaop-src->asm operator) x86:eax))]
+    [Num 
+     (b)
+     (x86:seqn
+      (x86:mov x86:eax b))]
+    [Id
+     (i)
+     (x86:seqn
+      (x86:mov x86:eax
+               ((match-lambda 
+                  [(cons 'stack-pos sp) 
+                   (x86:esp+ (* 4 (+ sp (stack-offset))))])
+                (hash-ref gamma (Id i)))))]))
+
+(define interp (λ (pp) (interp-p pp (hash))))
+(define (interp-p pp gamma)
+  (type-case P pp
+    [Program
+     (defs body)
+     (interp-k body
+               (for/fold ([g gamma])
+                 ([def defs])
+                 (hash-set g (Def-naming def) def)))]
+    [P-K (k) (interp-k k gamma)]))
+
+(define (interp-k pp gamma)
+  (type-case K pp
+    [If0 
+     (test trueb falsb)
+     (if (= 0 (interp-e test gamma))
+         (interp-k trueb gamma)
+         (interp-k falsb gamma))]
+    [App
+     (function inputs)
+     (let ([fd (hash-ref gamma function)])
+       (interp-k (Def-body fd)
+                 (for/fold ([g gamma])
+                   ([var (map Id-name (Def-variables fd))]
+                    [val (map (λ (i) (interp-e i gamma)) inputs)])
+                   (hash-set g var val))))]
+    [Return (e) (interp-e e  gamma)]))
+
+(define (interp-e pp gamma)
+  (type-case E pp
+    [Binop 
+     (opor lhs rhs)
+     ((binop-src->rkt opor)
+      (interp-e lhs gamma)
+      (interp-e rhs gamma))]
+    [Unaop 
+     (opor opand)
+     ((unaop-src->rkt opor)
+      (interp-e opand gamma))]
+    [Num (b) b]
+    [Id (i) (let ([v (hash-ref gamma i)])
+              (if (E? v) (interp-e v gamma) v))]))
 
 (provide
  (contract-out
-  [struct e ()]
-  [struct (num e) ([n byte?])]
-  [parse (-> any/c e?)]
-  [to-asm (-> e? x86:asm?)]
-  [interp (-> e? any/c)]))
+  [rename parse-p parse (-> any/c P?)]
+  [to-asm (-> P? x86:asm?)]
+  [interp (-> P? any/c)]))
