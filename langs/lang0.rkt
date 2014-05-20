a651ec36fd7d8634907354035254aa72bebe88f2
Trevor Oakes <trevor.oakes@gmail.com>
2014-05-20 14:00:22 -0600

    Minor: renamed dayN.rkt to langN.rkt

---
+++ langs/lang0.rkt
@@ -0,0 +1,33 @@
+#lang racket/base
+(require racket/contract
+         racket/match
+         plai/datatype
+         (prefix-in x86: "asm.rkt"))
+
+#| A valid program is a number [0,256), whose exit code, when run is that number
+   e = <num>
+|#
+
+(define-type E [num (n number?)]) ;XX instead of number? 32bitnum?
+
+(define parse
+  (match-lambda
+   [(? byte? b) ;XX instead of byte? 32bitnum?
+    (num b)]))
+
+(define (to-asm pp)
+  (type-case E pp
+   [num (b)
+    (x86:seqn
+     (x86:mov x86:eax b))]))
+
+(define (interp pp)
+  (type-case E pp
+   [num (b)
+    b]))
+
+(provide
+ (contract-out
+  [parse (-> any/c E?)]
+  [to-asm (-> E? x86:asm?)]
+  [interp (-> E? any/c)]))
---
+++ langs/lang2.rkt
@@ -0,0 +1,95 @@
+#lang racket/base
+(require racket/contract
+         racket/match
+         plai/datatype
+         "../substitution-cipher.rkt"
+         (prefix-in x86: "asm.rkt"))
+
+#| Add * quotient remainder to the binops
+   E = <num> | (<binop> E E) | (<unaop> E)
+   <binop> = + | - | bitwise-and | bitwise-ior | bitwise-xor
+             | * | quotient | remainder
+   <unaop> = add1 | sub1 | bitwise-not
+|#
+
+(define-type E 
+  [Num (n number?)]
+  [Binop (op binop-src?) (lhs E?) (rhs E?)]
+  [Unaop (opor unaop-src?) (opand E?)])
+
+(define (binop-src->asm-helper ->asm)
+  (match-lambda*
+    [(list x86:eax (? x86:register? r))
+     (->asm r)]))
+
+(define-subst-cipher
+  binop
+  [src          rkt         asm]
+  ['+           +           x86:add]
+  ['-           -           x86:sub]
+  ['bitwise-and bitwise-and x86:and]
+  ['bitwise-ior bitwise-ior x86:or]
+  ['bitwise-xor bitwise-xor x86:xor]
+  ['*           *           (binop-src->asm-helper x86:imul)]
+  ['quotient    quotient    (binop-src->asm-helper x86:idiv)]
+  ['remainder   remainder   (binop-src->asm-helper 
+                             (λ (r) 
+                               (x86:seqn
+                                (x86:idiv r)
+                                (x86:mov x86:eax x86:edx))))])
+
+(define-subst-cipher
+  unaop
+  [src          rkt         asm]
+  ['add1        add1        x86:inc]
+  ['sub1        sub1        x86:dec]
+  ['bitwise-not bitwise-not x86:not])
+
+(define parse
+  (match-lambda
+    [(list (? binop-src? operator) lhs rhs)
+     (Binop operator (parse lhs) (parse rhs))]
+    [(list (? unaop-src? operator) operand)
+     (Unaop operator (parse operand))]
+    [(? byte? b)
+     (Num b)]))
+
+(define (to-asm pp)
+  (type-case E pp
+    [Binop 
+     (op lhs rhs)
+     (x86:seqn
+      (x86:push x86:ebx)
+      (to-asm rhs)
+      (x86:mov x86:ebx x86:eax)
+      (to-asm lhs)
+      ((binop-src->asm op) x86:eax x86:ebx)
+      (x86:pop x86:ebx))]
+    [Unaop 
+     (operator operand)
+     (x86:seqn
+      (to-asm operand)
+      ((unaop-src->asm operator) x86:eax))]
+    [Num 
+     (b)
+     (x86:seqn
+      (x86:mov x86:eax b))]))
+
+(define (interp pp)
+  (type-case E pp
+    [Binop 
+     (opor lhs rhs)
+     ((binop-src->rkt opor)
+      (interp lhs)
+      (interp rhs))]
+    [Unaop 
+     (opor opand)
+     ((unaop-src->rkt opor)
+      (interp opand))]
+    [Num (b) b]))
+
+(provide
+ (contract-out
+  [parse (-> any/c E?)]
+  [to-asm (-> E? x86:asm?)]
+  [interp (-> E? any/c)]))
